f231e2a43415e3115c1f83ae9481e6a66c41cd33
Art Gonzalez <arturo.gonzalez@austincc.edu>
2018-09-20 22:53:05 -0500

    update basics

--- 01-basics/basics.h
+++ 01-basics/basics.h
@@ -3,5 +3,3 @@
 int multiply_numbers(int num1, int num2);
 
 void pass_by_val_by_ref(int val, int& ref);
-
-void const_pass_by_val_by_ref(int val, const int& ref);
--- 01-basics/main.cpp
+++ 01-basics/main.cpp
@@ -5,11 +5,7 @@ using std::cout;
 
 int main()
 {
-	//cout << sum_numbers(5, 5);
-	int num1{ 0 }, num2{ 0 };
-
-	const_pass_by_val_by_ref(num1, num2);
-	cout << num1 << " " << num2;
+	cout << sum_numbers(5, 5);
 
 	return 0;
 }
