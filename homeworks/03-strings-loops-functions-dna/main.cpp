44f224cea60a7016a54578b3b9c252b80e6230a1
Art Gonzalez <arturo.gonzalez@austincc.edu>
2018-09-21 14:42:29 -0500

    call dna functions

--- homeworks/03-strings-loops-functions-dna/main.cpp
+++ homeworks/03-strings-loops-functions-dna/main.cpp
@@ -1,14 +1,25 @@
-//write the dna.h include statement here
+#include "dna.h"
+#include<iostream>
+#include<string>
+
+using std::cout; using std::endl;
 
 int main()
 {
 	//call the dna functions from dna.h and display output
 	//Use the function arguments from the test cases in dna_test.cpp
 	//Example:
-	/*
-	auto point_mutation = get_point_mutations("GAGCCTACTAACGGGAT", "CATCGTAATGACGG")
-	cout<<point_mutation;
-	*/
+	auto point_mutation = get_point_mutations("GAGCCTACTAACGGGAT", "GAGCCTACTAACGGGAT");
+	cout << point_mutation << endl << endl;
+
+	auto complement = get_dna_complement("AAAACCCGGT");
+	cout << complement << endl << endl;
+
+	auto rna = transcribe_dna_into_rna("GATGGAACTTGACTACGTAAATT");
+	cout << rna << endl << endl;
+
+	auto gc_content = get_gc_content("CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT");
+	cout << gc_content << endl << endl;
 
 	return 0;
 }
