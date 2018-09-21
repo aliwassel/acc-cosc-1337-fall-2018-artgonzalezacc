a30344e577cadfd78476fe3d704ea229381e6117
Art Gonzalez <arturo.gonzalez@austincc.edu>
2018-09-21 14:29:20 -0500

    Homework 3 solution

--- homeworks/03-strings-loops-functions-dna-test/dna_test.cpp
+++ homeworks/03-strings-loops-functions-dna-test/dna_test.cpp
@@ -25,7 +25,17 @@ AAAACCCGGT result should be ACCGGGTTTT
 CCCGGAAAAT result should be ATTTTCCGGG
 */
 
+TEST_CASE("Test get dna complement") 
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
 
+TEST_CASE("TEST Reverse string") 
+{
+	REQUIRE(reverse_string("1234") == "4321");
+	REQUIRE(reverse_string("GAGCCTACTAACGGGAT") == "TAGGGCAATCATCCGAG");
+}
 
 
 /*
@@ -34,8 +44,11 @@ GATGGAACTTGACTACGTAAATT result should be GAUGGAACUUGACUACGUAAAUU
 AAATTGATCTTGACTACGTGGAA result should be AAAUUGAUCUUGACUACGUGGAA
 
 */
-
-
+TEST_CASE("Test trasncribe dna into rna") 
+{
+	REQUIRE(transcribe_dna_into_rna("GATGGAACTTGACTACGTAAATT") =="GAUGGAACUUGACUACGUAAAUU"	);
+	REQUIRE(transcribe_dna_into_rna("AAATTGATCTTGACTACGTGGAA") == "AAAUUGAUCUUGACUACGUGGAA");
+}
 
 
 /*create a test case for function get_gc_content with arguments
--- homeworks/03-strings-loops-functions-dna/dna.cpp
+++ homeworks/03-strings-loops-functions-dna/dna.cpp
@@ -1,22 +1,84 @@
 #include<math.h>
 #include "dna.h"
 
-int get_point_mutations(std::string dna1, std::string dna2)
-{	//write the function code 
+using std::string;
+
+int get_point_mutations(const string& dna1, const string& dna2)
+{	
+	if (dna1.size() != dna2.size()) 
+	{
+		return -1;
+	}
 	
-	return 0;
+	int hamming_distance = 0;
+
+	for (std::size_t i = 0; i < dna1.size(); ++i) 
+	{
+		if (dna1[i] != dna2[i]) 
+		{
+			hamming_distance++;
+		}
+	}
+
+	return hamming_distance;
 }
 
 
 //write function code for std::string get_dna_complement(std::string dna)
+string get_dna_complement(string dna) 
+{
 
+	for (std::size_t i = 0; i < dna.size(); ++i) 
+	{
+		switch (dna[i]) 
+		{
+		case 'A':
+			dna[i] = 'T';
+			break;
+		case 'T':
+			dna[i] = 'A';
+			break;
+		case 'C':
+			dna[i] = 'G';
+			break;
+		case 'G':
+			dna[i] = 'C';
+			break;
+		default:
+			break;
+		}
+	}
 
+	return reverse_string(dna);
+}
+
+string reverse_string(const string& str)
+{
+	std::string reverse;
+	
+	for (std::size_t i = str.size()-1; i != -1; --i)
+	{
+		reverse.push_back(str[i]);
+	}
+
+	return reverse;
+}
 
 //write function code for std::string transcribe_dna_into_rna(std::string dna);
+string transcribe_dna_into_rna(string dna)
+{
+	for (auto& s : dna) 
+	{
+		if (s == 'T') 
+		{
+			s = 'U';
+		}
+	}
 
+	return dna;
+}
 
-
-double get_gc_content(std::string dna)
+double get_gc_content(const string& dna)
 {
 	auto size = dna.size(); //get the size of string let auto handle the data type
 	auto gc_count{ 0 }; //initialize to 0 (int) let auto handle data type
@@ -28,7 +90,10 @@ double get_gc_content(std::string dna)
 		//in C++ logical and is && and logical or is ||
 		//usage (and) condition1 && condition2 --- usage (or) condition1 || condition2 
 		//write code to determine if s is 'C' or 'G' then increment gc_count by 1
-
+		if (s == 'C' || s == 'G') 
+		{
+			gc_count++;
+		}
 
 	}
 
